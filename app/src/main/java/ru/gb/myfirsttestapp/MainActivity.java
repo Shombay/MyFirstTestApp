f45a2a397d2d8424df9b3e0f5b133d078b5dfbaf
shombay <shaiba1989.almet@gmail.com>
2021-12-12 23:38:37 +0300

    HomeWork1.6 change Button

--- app/src/main/java/ru/gb/myfirsttestapp/MainActivity.java
+++ app/src/main/java/ru/gb/myfirsttestapp/MainActivity.java
@@ -11,26 +11,26 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private TextView nameTV;
-//    private Button changeNameButton;
+    private TextView nameTV;
+    private Button changeNameButton;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.new_activity_main);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.new_activity_main);
 
-//
-//        nameTV = findViewById(R.id.name_text_view);
-//        changeNameButton = findViewById(R.id.change_name_text_view);
-//
-//        changeNameButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                nameTV.setText("Well Done");
-//            }
-//        });
+
+        nameTV = findViewById(R.id.name_text_view);
+        changeNameButton = findViewById(R.id.change_name_text_view);
+
+        changeNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nameTV.setText("Well Done");
+            }
+        });
     }
     }
